c122065be356511ea0436cc886dd4791a10e7b28
Adrian Schneider <leftshiftlp@gmail.com>
2017-06-23 18:16:39 +0200

    moved pattern getting to utils

--- utils.py
+++ utils.py
@@ -10,12 +10,29 @@ INSTRUMENT_CHOICES = collections.OrderedDict((
     ('guitardd','guitar (drop d)')))
 FLATS_TO_SHARPS = {'db':'c#', 'eb':'d#', 'gb':'f#', 'bb': 'a#'}
 
-def diffs(items): #-1 for X
+
+def get_chords(STRINGS, CHORDS, name, max_fingers=10, with_inversions=False,
+               same_shapes=False):
+    by_diff = build_diff_dict(CHORDS)
+    name = name.lower()
+    one = FLATS_TO_SHARPS.get(name, name)
+
+    matches = [(n,p) for n,p in CHORDS if any([n.lower()==one, n[:len(one)+1].lower()==one+'/'])]
+
+    if max_fingers:
+        matches = [(n,p) for n,p in matches if len([x for x in p if x > 0]) <= args.max_fingers]
+
+    return matches[:(1,-1)[with_inversions]]
+
+
+def diffs(items):  # -1 for X
     items = [i for i in items if i > -1]
     return [j-i for i, j in zip(items[:-1], items[1:])]
 
+
 def shape_to_diff_id(pattern):
     return ''.join(map(str, diffs(pattern)))
+
 
 def build_diff_dict(chords):
     diff = collections.defaultdict(list)
@@ -23,9 +40,11 @@ def build_diff_dict(chords):
         diff[shape_to_diff_id(pattern)].append((chord, pattern))
     return diff
 
+
 def with_same_pattern(pattern, by_diff):
     ret = [x for x in by_diff.get(shape_to_diff_id(pattern)) if x[1]!=pattern]
     return ret or []
+
 
 def render(pattern, strings, padd=0):
     DIM, DIM_RESET = '\033[2m', '\033[22m'
@@ -41,6 +60,7 @@ def render(pattern, strings, padd=0):
         line = ('X' if muted else '|') + line[1:]
         print(' ' * padd + '%s%s %s|%s' % (('',DIM)[muted], string,
                                            ''.join(line), ('',DIM_RESET)[muted]))
+
 
 def get_instrument(instrument):
     instrument = importlib.import_module(instrument)
